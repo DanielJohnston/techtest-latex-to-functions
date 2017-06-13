d1d79a20d03f08b82f2bb32fdd9d4281f4bce09e
Daniel Whitston <dan@whitston.org.uk>
2017-06-13 14:52:16 +0100

    multiplication tests passing

--- lib/string.rb
+++ lib/string.rb
@@ -26,10 +26,30 @@ class String
     term_matcher = '[\+|\-][0-9a-z]+'
     terms = self.scan(/#{term_matcher}/)
 
-    # Create an addition object if the top level is an addition
+    # Create an addition object if the top level has addition operations
     unless terms.length == 1
-      terms.map!{|term| term.objectify}
+      terms.map!{ |term| term.objectify }
       return Addition.new(terms)
+    end
+
+    # If the top level isn't addition, break into multiplication factors
+    sign, term = terms[0][0], terms[0][1..-1]
+    factor_matcher = '[a-z]|[0-9]+'
+    factors = term.scan(/#{factor_matcher}/)
+
+    # Move a negative sign into the factorisation, where e.g. -a => -1 x a
+    if sign == '-'
+      if factors[0][/[0-9]+/]
+        factors[0] = '-' + factors[0]
+      else
+        factors.unshift('-1')
+      end
+    end
+
+    # Return a multiplication object if the top level has multiple factors
+    unless factors.length == 1
+      factors.map!{ |factor| factor.objectify }
+      return Multiplication.new(factors)
     end
 
     # Match all bracketed terms: \((?>[^)(]+|\g<0>)*\)
